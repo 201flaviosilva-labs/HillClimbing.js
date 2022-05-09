30e0df1f6a8ab2917909e3e2f363e10bdf8c9343
Flavio Silva <55815265+201flaviosilva@users.noreply.github.com>
2022-05-09 14:32:03 +0100

    Document code based in the JS Doc #1
    https://github.com/201flaviosilva/HillClimbing.js/issues/1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+/**
+ * @classdesc 
+ * A entry point for the Hill Climbing algorithm.
+ * 
+ * @example
+ * var hillClimbing = new HillClimbing([
+ * const targets = [
+ * {
+ * 	name: "myValue1", // The name of the value
+ * 	value: 50, // The initial value
+ * 	min: 0, // The minimum value that the value can be
+ * 	max: 100, // The maximum value that the value can be
+ * },
+ * { name: "myValue2", value: -2, min: -100, max: 10 },
+ * { name: "myValue3", value: 764, min: 100, max: 1250 },
+ * ];
+ * 
+ * const myHillClimbing = new HillClimbing(targets); // Create a new instance and pass the initial data (targets)
+ * 
+ * @class HillClimbing
+
+ * @param {Array} targets - a list of targets
+ * @constructor
+ * @see {@link https://en.wikipedia.org/wiki/Hill_climbing}
+ */
 class HillClimbing {
 	constructor(targets) {
 		this.targets = targets;
@@ -11,36 +36,201 @@ class HillClimbing {
 		this.bestScore = -Infinity;
 	}
 
+	/**
+	 * @description 
+	 * Add a new target to the list of targets
+	 * 
+	 * @example
+	 * myHillClimbing.addTarget({ name: "myNewValue", value: 50, min: 0, max: 100 });
+	 * 
+	 * @param {Object} target - A new target to calculate in the new solution
+	 * @returns {void}
+	 * @memberof HillClimbing
+	 */
 	addTarget(target) {
 		this.targets.push(target);
 		this.bestSolution.push(target);
 		this.currentSolution.push(target);
 	}
+
+	/**
+	 * @description 
+	 * Change all targets to the new targets
+	 * 
+	 * @example
+	 * myHillClimbing.setAllTargets([
+	 * { name: "myNewValue1", value: 0, min: 0, max: 5 },
+	 * { name: "myNewValue2", value: -100, min: -500, max: -100 },
+	 * { name: "myNewValue3", value: 0, min: 0, max: 1 },
+	 * ]);
+	 * 
+	 * @param {Array} targets - A list of new targets
+	 * @returns {void}
+	 * @memberof HillClimbing
+	 */
 	setAllTargets(targets) {
 		this.targets = targets;
 		this.bestSolution = targets;
 		this.currentSolution = targets;
 	}
-	removeTarget(target) {
-		this.targets.splice(this.bestSolution.indexOf(target), 1);
-		this.bestSolution.splice(this.bestSolution.indexOf(target), 1);
-		this.currentSolution.splice(this.currentSolution.indexOf(target), 1);
+
+	/**
+	 * @description
+	 * Remove a target from the list of targets
+	 * 
+	 * @example
+	 * myHillClimbing.removeTarget("myValue1");
+	 * 
+	 * @param {String} name - The name of the target to remove
+	 * @returns {void}
+	 * @memberof HillClimbing
+	 */
+	removeTarget(name) {
+		const index = this.targets.findIndex(target => target.name === name);
+		if (index > -1) {
+			this.targets.splice(index, 1);
+			this.bestSolution.splice(index, 1);
+			this.currentSolution.splice(index, 1);
+		}
 	}
 
+	/**
+	 * @description
+	 * Returns the number of iterations that have been run
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getNumberOfIterations());
+	 * 
+	 * @returns {Number} - The number of iterations that have been run
+	 * @memberof HillClimbing
+	 */
 	getNumberOfIterations() { return this.numberOfIterations; }
+
+	/**
+	 * @description
+	 * Get the best score that has been found so far
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getBestScore());
+	 * 
+	 * @returns {Number} - The best score that has been found so far
+	 * @memberof HillClimbing
+	 */
 	getBestScore() { return this.bestScore; }
 
+	/**
+	 * @description
+	 * Returns all targets from the list
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getTargets());
+	 * 
+	 * @returns {Array} - The list of targets
+	 * @memberof HillClimbing
+	 */
 	getTargets() { return this.targets; }
+
+	/**
+	 * @description
+	 * Returns the current best solution. The best solution is the solution that has the highest score
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getBestSolution());
+	 * 
+	 * @returns {Array} - The best solution
+	 * @memberof HillClimbing
+	 */
 	getBestSolution() { return this.bestSolution; }
+
+	/**
+	 * @description
+	 * Returns a array with the values of the targets with the best solution
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getBestSolutionValues());
+	 * 
+	 * @returns {Array} - The values of the targets with the best solution
+	 * @memberof HillClimbing
+	 */
 	getBestSolutionValues() { return this.bestSolution.map(target => target.value); }
+
+	/**
+	 * @description
+	 * Returns the current solution
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getCurrentSolution());
+	 * 
+	 * @returns {Array} - The current solution
+	 * @memberof HillClimbing
+	 */
 	getCurrentSolution() { return this.currentSolution; }
+
+	/**
+	 * @description
+	 * Returns a array with the values of the targets with the current solution
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getCurrentSolutionValues());
+	 * 
+	 * @returns {Array} - The values of the targets with the current solution
+	 * @memberof HillClimbing
+	 */
 	getCurrentSolutionValues() { return this.currentSolution.map(target => target.value); }
 
+	/**
+	 * @description
+	 * Returns the current solution value of the given target name
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getCurrentSolutionValue("myValue1"));
+	 * 
+	 * @param {String} name - The name of the target
+	 * @returns {Number} - The current solution value of the given target name
+	 * @memberof HillClimbing
+	 */
 	getCurrentTargetValueSolutionByName(name) { return this.currentSolution.find(target => target.name === name).value; }
+
+	/**
+	 * @description
+	 * Returns the best solution value of the given target name
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getBestSolutionValue("myValue1"));
+	 * 
+	 * @param {String} name - The name of the target
+	 * @returns {Number} - The best solution value of the given target name
+	 * @memberof HillClimbing
+	 */
 	getBestTargetValueSolutionByName(name) { return this.bestSolution.find(target => target.name === name).value; }
 
+	/**
+	 * @description
+	 * Returns the last target that has been changed
+	 * 
+	 * @example
+	 * console.log(myHillClimbing.getLastTargetChanged());
+	 * 
+	 * @returns {Object} - The last target that has been changed
+	 * @memberof HillClimbing
+	 */
 	getLastTargetChanged() { return this.lastTargetChanged; }
 
+	/**
+	 * @description
+	 * This function its the main function of the algorithm.
+	 * This function will calculate a new solution based on the best solution and will return the new solution.
+	 * 
+	 * Based in the given score, the algorithm will change randomly the value of a random target.
+	 *
+	 * @example
+	 * const myNewScore = 10;
+	 * myHillClimbing.run(myNewScore);
+	 * 
+	 * @param {Number} score - The score that will be used to calculate the new solution
+	 * @returns {Array} - The new current solution
+	 * @memberof HillClimbing
+	 */
 	run(score) {
 		this.numberOfIterations++;
 		this.lastScore = score;
@@ -63,6 +253,17 @@ class HillClimbing {
 		return this.currentSolution;
 	}
 
+	/**
+	 * @description
+	 * Resets the algorithm
+	 * 
+	 * @example
+	 * myHillClimbing.reset();
+	 * 
+	 * @memberof HillClimbing
+	 * @returns {void}
+	 * @memberof HillClimbing
+	 */
 	reset() {
 		this.numberOfIterations = 0;
 		this.bestSolution = this.targets;
@@ -71,9 +272,32 @@ class HillClimbing {
 		this.bestScore = -Infinity;
 	}
 
+	/**
+	 * @description 
+	 * Returns teh current version of the library
+	 * 
+	 * @example
+	 * console.log(HillClimbing.getVersion());
+	 * 
+	 * @returns {String}
+	 * @memberof HillClimbing
+	 * @method getVersion
+	 */
 	getVersion() { return "0.0.0"; }
 
-	// --- Utils
+	/**
+	 * @description 
+	 * Get a random number between two numbers
+	 * 
+	 * @example
+	 * myHillClimbing.randomNumber(0, 100);
+	 * 
+	 * @param {Number} min - The minimum number
+	 * @param {Number} max - The maximum number
+	 * @returns {Number} - The random number
+	 * @memberof HillClimbing
+	 * @method randomNumber
+	 */
 	randomNumber(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
 }
 
